0bb8bcfc86205690a31256b1b9c4b67362be1608
Andy Ackland <andy.ackland@gmail.com>
2025-04-12 10:24:51 +0100

    Fix invalid escape sequence See #1 (Add materials to assist in setting up a development environment)

--- tools/w64/create_w64_dev_env_bootstrap.py
+++ tools/w64/create_w64_dev_env_bootstrap.py
@@ -367,10 +367,10 @@ dockerfile.write_text(dockerfile_text)
 run_all_tests_text = """\
 Write-Host "Running full install + test + teardown + verify sequence..."
 
-.\lcapy-w64-dev-env-setup.ps1
-.\lcapy-w64-dev-env-test.ps1
-.\lcapy-w64-dev-env-teardown.ps1
-.\lcapy-w64-dev-env-test-teardown.ps1
+.\\lcapy-w64-dev-env-setup.ps1
+.\\lcapy-w64-dev-env-test.ps1
+.\\lcapy-w64-dev-env-teardown.ps1
+.\\lcapy-w64-dev-env-test-teardown.ps1
 
 Write-Host "All phases completed."
 """
